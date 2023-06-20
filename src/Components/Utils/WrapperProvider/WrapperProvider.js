2f8c9d64ef979ffa75ad13899fc755df26f4299d
Joaquin Gribodo <joacogribodo@gmail.com>
2023-06-20 19:24:53 -0300

    Providers

---
+++ src/Components/Utils/WrapperProvider/WrapperProvider.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { ThemeContextProvider } from "./Components/Services/Theme/Theme.Context";
+import { APIContextProvider } from "./Components/Services/API/Api.Context";
+import { CartContextProvider } from "./Components/Services/Cart/Cart.Context";
+import { UsersContextProvider } from "./Components/Services/Users/Users.Context";
+
+const WrapperProvider = ({ children }) => {
+  return (
+    <UsersContextProvider>
+      <ThemeContextProvider>
+        <APIContextProvider>
+          <CartContextProvider>{children}</CartContextProvider>
+        </APIContextProvider>
+      </ThemeContextProvider>
+    </UsersContextProvider>
+  );
+};
+
+export default WrapperProvider;
--- src/index.js
+++ src/index.js
@@ -2,22 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ThemeContextProvider } from "./Components/Services/Theme/Theme.Context";
-import { APIContextProvider } from "./Components/Services/API/Api.Context";
-import { CartContextProvider } from "./Components/Services/Cart/Cart.Context";
-import { UsersContextProvider } from "./Components/Services/Users/Users.Context";
+import WrapperProvider from "./Components/Utils/WrapperProvider/WrapperProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <UsersContextProvider>
-    <ThemeContextProvider>
-      <APIContextProvider>
-        <CartContextProvider>
-          <App />
-        </CartContextProvider>
-      </APIContextProvider>
-    </ThemeContextProvider>
-    </UsersContextProvider>
+    <WrapperProvider>
+      <App />
+    </WrapperProvider>
   </React.StrictMode>
 );
